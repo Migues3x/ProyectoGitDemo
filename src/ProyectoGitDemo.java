db6f8a86730aef755d9940f3fd872ac824f181b8
Cass <gersonrojasmarca@yahoo.com>
2025-04-27 18:32:46 -0500

    Agregar método contar letras y números

--- src/ProyectoGitDemo.java
+++ src/ProyectoGitDemo.java
@@ -1,19 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author User
- */
 public class ProyectoGitDemo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String contarLetrasYNumeros(String cadena) {
+        int letras = 0;
+        int numeros = 0;
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            if (Character.isLetter(c)) {
+                letras++;
+            } else if (Character.isDigit(c)) {
+                numeros++;
+            }
+        }
+
+        return "Cantidad de letras: " + letras + "\n" +
+               "Cantidad de números: " + numeros;
     }
     
 }
